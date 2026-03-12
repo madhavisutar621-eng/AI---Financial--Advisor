0f767e71f50ee8332e425413b17025d00e34f043
madhavisutar621-eng <madhavisutar621@gmail.com>
2026-03-12 19:12:43 +0530

    Update Main_app.py

--- Main_app.py
+++ Main_app.py
@@ -1,4 +1,18 @@
 import streamlit as st
 
-st.title("Hello! My Streamlit App is Working")
-st.write("AI Financial Advisor App")
+st.title("AI Financial Advisor")
+
+income = st.number_input("Enter your monthly income")
+
+expenses = st.number_input("Enter your monthly expenses")
+
+if st.button("Calculate"):
+    savings = income - expenses
+    st.write("Your savings:", savings)
+
+    if savings > 5000:
+        st.success("Great! You can invest in mutual funds or SIP.")
+    elif savings > 0:
+        st.info("Try to save more and start small investments.")
+    else:
+        st.error("Your expenses are higher than income.")
